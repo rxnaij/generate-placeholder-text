42ff5c667c679b83e2881e4b7f52aa0b45be06dd
rxnaij <richardblue37@gmail.com>
2021-09-23 17:37:44 -0400

    implement basic placeholder text generation

--- code.ts
+++ code.ts
@@ -1,20 +1,26 @@
-// This plugin creates 5 rectangles on the screen.
-const numberOfRectangles = 5
+const placeholderText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
+// if text node isn't currently selected
+// just create a new text node with placeholder text
 
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
+const currentSelection = figma.currentPage.selection[0]
+
+if (currentSelection.type === 'TEXT') {
+  if (!currentSelection.hasMissingFont) {
+    // Check for if all fonts are loaded first
+    const fonts = currentSelection.getRangeAllFontNames(0, currentSelection.characters.length)
+    Promise.all(fonts.map(font => figma.loadFontAsync(font)))
+      .then(() => {
+        currentSelection.characters = placeholderText
+      })
+      .catch(error => {
+        console.error(error)
+        throw new Error(`Error: Could not load fonts`)
+      })
+  }
 }
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
+
+// Edge case: what happens if multiple layers are selected?
 
 // Make sure to close the plugin when you're done. Otherwise the plugin will
 // keep running, which shows the cancel button at the bottom of the screen.

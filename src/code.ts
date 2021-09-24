049f94224b8d477a573cfbe99bc9c48181df9aa3
rxnaij <richardblue37@gmail.com>
2021-09-23 21:01:56 -0400

    add webpack bundling

--- src/code.ts
+++ src/code.ts
@@ -1,7 +1,5 @@
 const placeholderText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
 
-// if text node isn't currently selected
-// just create a new text node with placeholder text
 
 const currentSelection = figma.currentPage.selection[0]
 
@@ -20,8 +18,8 @@ if (!currentSelection) {
     node.y = figma.viewport.center.y
   })
 } else if (currentSelection.type === 'TEXT') {
+  // Check if all fonts are loaded first
   if (!currentSelection.hasMissingFont) {
-    // Check for if all fonts are loaded first
     const fonts = currentSelection.getRangeAllFontNames(0, currentSelection.characters.length)
     Promise.all(fonts.map(font => figma.loadFontAsync(font)))
       .then(() => {
